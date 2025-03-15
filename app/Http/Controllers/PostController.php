f464e41754b91ea2b56e4a741359893b4cfd7d6f
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-03-15 22:03:28 +0000

    ajouter une controller des post avec une function de create post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'content' => ['required', 'string'],
+            'image' => ['required', 'string']
+        ]);
+
+        $post = Post::create([
+            'user_id' => auth()->id(),
+            'content' => $request->input('content'),
+            'image' => $request->input('image'),
+        ]);
+
+        return response()->json([
+            'message' => 'Post ajouté avec succès !',
+            'post' => $post
+        ], 201); 
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
