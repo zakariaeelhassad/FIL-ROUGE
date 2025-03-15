6b57e0f44b37ac9e720343b8e364fdff5567fb64
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-03-15 22:04:20 +0000

    ajouter une route de function store des posts

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::post('/register' , [AuthController::class , 'register']);
+Route::group(['middleware'=>['auth:sanctum']], function(){
+    Route::post('/post', [PostController::class , 'store']);
+});
