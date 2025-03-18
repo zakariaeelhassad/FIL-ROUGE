1de774e279dde36b042cc8af60860438c312b596
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-03-18 12:28:57 +0000

    j'ai faire des modification dans migrate de like

--- database/migrations/2025_03_15_224452_create_likes_table.php.php
+++ database/migrations/2025_03_15_224452_create_likes_table.php.php
@@ -10,11 +10,9 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('post_id')->constrained()->onDelete('cascade'); 
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
+            $table->morphs('likeable'); 
             $table->timestamps();
-
-            $table->unique(['user_id', 'post_id']);
         });
     }
 
