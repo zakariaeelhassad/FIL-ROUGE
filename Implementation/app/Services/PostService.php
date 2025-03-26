4c16017f32a7d74251c0cfcf999119d7b9f72236
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-03-26 10:38:30 +0000

    Créer une fichier PostService.

---
+++ Implementation/app/Services/PostService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\interface\RepositoryInterface;
+use App\Repositories\PostRepository;
+
+class PostService
+{
+    private PostRepository $repository;
+
+    public function __construct(PostRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    public function create(array $data)
+    {
+        $data['user_id'] = auth()->id();
+        return $this->repository->create($data);
+    }
+
+    public function update(array $data, int $id)
+    {
+        return $this->repository->update($data, $id);
+    }
+
+    public function delete(int $id)
+    {
+        $this->repository->delete($id);
+    }
+
+    public function all()
+    {
+        return $this->repository->all();
+    }
+
+    public function find(int $id)
+    {
+        return $this->repository->find($id);
+    }
+}
