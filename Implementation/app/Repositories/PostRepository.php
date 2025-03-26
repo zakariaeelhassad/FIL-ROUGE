fc0a4e9101884f8f839882b765972ad4ca22b71c
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-03-26 10:36:27 +0000

    Créer une fichier CommenterRepository .

---
+++ Implementation/app/Repositories/PostRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Post;
+use App\Repositories\interface\RepositoryInterface;
+
+class PostRepository implements RepositoryInterface
+{
+    public function all()
+    {
+        return Post::all();
+    }
+
+    public function create(array $data)
+    {
+        return Post::create($data);
+    }
+
+    public function update(array $data, int $id)
+    {
+        $post = Post::findOrFail($id);
+
+        return $post->update($data);
+    }
+
+    public function delete(int $id)
+    {
+        $post = Post::findOrFail($id);
+
+        return $post->delete();
+    }
+
+    public function find(int $id)
+    {
+        return Post::find($id);
+    }
+}
