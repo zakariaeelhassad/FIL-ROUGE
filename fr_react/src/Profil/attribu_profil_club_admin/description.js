6a1f6e63c7d2082850f2fab65b99cf450876ea42
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-04-07 12:10:26 +0100

    ajouter une composant description dans le dossier attribu_profil_club_admin

---
+++ fr_react/src/Profil/attribu_profil_club_admin/description.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const DescriptionSection = () => {
+    return (
+        <div className="mt-4 border-2 border-blue-500 rounded-xl p-4">
+            <div className="flex justify-between items-center mb-3">
+                <h2 className="font-bold">Description</h2>
+                <button className="px-3 py-1 border border-blue-500 text-blue-500 rounded-full text-sm">edit</button>
+            </div>
+            <p className="text-gray-700 text-sm">
+                nefjhf rehrr fcfblehrfbf bjrejhbe n jkerf rejkbfrjrb jbrekjbfr jbrekjb jbkrkjbfe rejfbkjef jb khlob jbkjbk jbkjbk jb bkjbj jbkjb jbkjbk bjbjbk jbkvje vkbfvi aebkjbfaek ebfbez bezjkjbfz bzekbfz bezfze vjkbvf.
+            </p>
+        </div>
+    );
+};
+
+export default DescriptionSection;
