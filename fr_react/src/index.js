a27a90051d021126c257b1584db7decb9c84459b
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-04-08 17:16:36 +0100

    supprimer la route de navbar dans index

--- fr_react/src/index.js
+++ fr_react/src/index.js
@@ -4,17 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from "./navbar";  // تأكد من أنك تستورد BrowserRouter هنا
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter> {}
-            <Navbar />
-                <App />
+        <BrowserRouter>
+            <App />
         </BrowserRouter>
     </React.StrictMode>
 );
 
 reportWebVitals();
+
 
