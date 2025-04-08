44ffc3735c54276e62e11852a55b987e462e391d
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-04-08 17:18:14 +0100

    Affichage global de la Navbar sauf sur les pages Login et Signup

--- fr_react/src/App.js
+++ fr_react/src/App.js
@@ -1,25 +1,39 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from "./Home/home";
-import Profil_club_manager from "./Profil/profil_club_manager";
-import Profil_joueur from "./Profil/profil_joueur";
-import Reseau from "./reseau/reseau";
-import Notification from "./notification/notification";
-import Login from './auth/login';
-import Signup from './auth/signup';
-
-
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Home from "./page/home";
+import Reseau from "./page/reseau";
+import Login from './page/login';
+import Signup from './page/signup';
+import Navbar from './component/navbar';
+import Profil_club_manager from './page/profil_club_manager';
+import Profil_joueur from './page/profil_joueur';
+import Notification from './page/notification';
 
 function App() {
+    const location = useLocation();
+
+    const showNavbarPaths = [
+        "/home",
+        "/profil_club",
+        "/profil_joueur",
+        "/reseau",
+        "/notification"
+    ];
+
+    const showNavbar = showNavbarPaths.includes(location.pathname);
+
     return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profil_club" element={<Profil_club_manager />} />
-            <Route path="/profil_joueur" element={<Profil_joueur />} />
-            <Route path="/reseau" element={<Reseau />} />
-            <Route path="/notification" element={<Notification />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-        </Routes>
+        <>
+            {showNavbar && <Navbar />}
+            <Routes>
+                <Route path="/home" element={<Home />} />
+                <Route path="/profil_club" element={<Profil_club_manager />} />
+                <Route path="/profil_joueur" element={<Profil_joueur />} />
+                <Route path="/reseau" element={<Reseau />} />
+                <Route path="/notification" element={<Notification />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+            </Routes>
+        </>
     );
 }
 
