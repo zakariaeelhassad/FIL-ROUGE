36cca14df53cca9adb07a61692886cc912a6589d
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-04-08 17:15:47 +0100

    faire des modification dans axiosConfig

--- fr_react/src/axiosConfig.js
+++ fr_react/src/axiosConfig.js
@@ -1,11 +1,27 @@
-import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api", // URL ديال API ديالك فـ Laravel
+
+// src/utils/axios.js
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: "http://127.0.0.1:8000/api", 
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
     },
 });
 
-export default api;
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
