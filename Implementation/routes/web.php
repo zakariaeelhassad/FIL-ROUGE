8989e2097e2bb864ee0ddb17fb2a0a03df637263
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-04-11 15:02:16 +0100

    ajouter des route pour l'affichage des page

--- Implementation/routes/web.php
+++ Implementation/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+Route::get('/profil_joueur', function () {
+    return view('pages.profil_joueur');
 });
+
+
+Route::get('/profil_Club', function () {
+    return view('pages.profil_club_manager');
+});
+
+Route::get('/reseau', function () {
+    return view('pages.reseau');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/reseau', [UserController::class, 'reseau'])->name('reseau');
+});
+
+Route::get('/notification', function () {
+    return view('pages.notification');
+});
+
+
+Route::get('/signup', [UserController::class, 'create'])->name('signup');
+Route::post('/signup', [UserController::class, 'store'])->name('signup.store');
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+Route::get('/', [PostsController::class, 'index'])->name('posts.index');
+
+Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout')
+    ->middleware('auth');
