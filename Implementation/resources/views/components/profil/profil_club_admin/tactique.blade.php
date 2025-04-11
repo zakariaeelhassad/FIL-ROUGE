da8d486c1a0333669e92eba78682b4615a7b0713
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-04-11 14:39:12 +0100

    Ajout du layout Tactique dans le dossier profil_club_admin/profil/components

---
+++ Implementation/resources/views/components/profil/profil_club_admin/tactique.blade.php
@@ -0,0 +1,9 @@
+<div class="mt-4 border-2 border-blue-500 rounded-xl p-4">
+    <div class="flex justify-between items-center mb-3">
+        <h2 class="font-bold">Tactique</h2>
+        <button class="px-3 py-1 border border-blue-500 text-blue-500 rounded-full text-sm">Edit</button>
+    </div>
+    <p class="text-gray-700 text-sm">
+        nefjhf rehrr fcfblehrfbf bjrejhbe n jkerf rejkbfrjrb jbrekjbfr jbrekjb jbkrkjbfe rejfbkjef jb khlob jbkjbk jbkjbk jb bkjbj jbkjb jbkjbk bjbjbk jbkvje vkbfvi aebkjbfaek ebfbez bezjkjbfz bzekbfz bezfze vjkbvf.
+    </p>
+</div>
