2c8ca7e5bdb524da2f6462a75878e8c3580e1586
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-04-11 15:05:10 +0100

    ajouter une function setPasswordAttribute dans model User

--- Implementation/app/Models/User.php
+++ Implementation/app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -38,6 +39,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
 
     public function posts()
     {
