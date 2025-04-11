74befc2d6ee9631d80c465a57cff3f004b7fc608
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-04-11 15:04:34 +0100

    ajouter une function user dans model Post

--- Implementation/app/Models/Post.php
+++ Implementation/app/Models/Post.php
@@ -13,13 +13,20 @@ class Post extends Model
 
     protected $fillable = ['content' , 'image','user_id'];
 
+    public function user()
+    {
+        return $this->belongsTo(User::class); 
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class); 
+    }
+
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
 
-    public function likes()
-    {
-        return $this->morphMany(Like::class, 'likeable');
-    }
+
 }
