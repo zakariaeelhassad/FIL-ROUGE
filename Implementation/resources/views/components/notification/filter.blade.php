a85ffa82b74b2621a338d78cfa1aa9e38fd480bb
zakariae el hassad <zakariaelhassad031@gmail.com>
2025-04-11 14:31:19 +0100

    Création d'un nouveau layout appelé Filter pour gérer les filtres de notification

---
+++ Implementation/resources/views/components/notification/filter.blade.php
@@ -0,0 +1,14 @@
+<div class="flex justify-center mb-6">
+    <div class="bg-white border-2 border-blue-400 rounded-full p-1 w-full max-w-xl flex justify-between">
+      <button class="px-6 py-2 rounded-full bg-white text-gray-800 hover:bg-gray-100 focus:outline-none">
+        Toutes
+      </button>
+      <button class="px-6 py-2 rounded-full bg-white text-gray-800 hover:bg-gray-100 focus:outline-none">
+        Mes Posts
+      </button>
+      <button class="px-6 py-2 rounded-full bg-white text-gray-800 hover:bg-gray-100 focus:outline-none">
+        Offres
+      </button>
+    </div>
+  </div>
+  
