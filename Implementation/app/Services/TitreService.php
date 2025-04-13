93899694712a08d028bec32859d1a50c1e440c90
Zakariae El Hassad <zakariaelhassad031@example.com>
2025-04-13 20:26:44 +0100

    ajouter le ritreService dans Service

---
+++ Implementation/app/Services/TitreService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\TitreRepository;
+
+class TitreService
+{
+    private TitreRepository $repository;
+
+    public function __construct(TitreRepository $repository) {
+        $this->repository = $repository;
+    }
+
+    public function create(array $data)
+    {
+        $data['user_id'] = auth()->id();
+        return $this->repository->create($data);
+    }
+
+    public function update(array $data, int $id)
+    {
+        return $this->repository->update($data, $id);
+    }
+
+    public function delete(int $id)
+    {
+        $this->repository->delete($id);
+    }
+
+    public function all()
+    {
+         return $this->repository->all()->load('user');
+    }
+
+    public function find(int $id)
+    {
+        return $this->repository->find($id);
+    }
+}
