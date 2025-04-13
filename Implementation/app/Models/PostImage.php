0341906d1a45f00214dec2a3bd7d0ca536ab63b6
Zakariae El Hassad <zakariaelhassad031@example.com>
2025-04-13 20:17:11 +0100

    ajouter une model des postImage

---
+++ Implementation/app/Models/PostImage.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PostImage extends Model
+{
+    protected $fillable = ['post_id', 'path'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
