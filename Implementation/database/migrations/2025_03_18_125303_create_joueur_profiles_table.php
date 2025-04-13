3610bfb9811454db44d4d95a1777275e188791b7
Zakariae El Hassad <zakariaelhassad031@example.com>
2025-04-13 20:13:59 +0100

    Modifie le name de foreifnid de user dans migration de joueur_profiles

--- Implementation/database/migrations/2025_03_18_125303_create_joueur_profiles_table.php
+++ Implementation/database/migrations/2025_03_18_125303_create_joueur_profiles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('joueur_profiles', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });        
     }
