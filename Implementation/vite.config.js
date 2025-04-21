f912c8d29bc1a1695f4f0d394ec2dcf45e7e88bb
zakariaeelhassad <zakariaelhassad031@example.com>
2025-04-21 16:20:23 +0100

    modifier le vite.config.js

--- Implementation/vite.config.js
+++ Implementation/vite.config.js
@@ -4,8 +4,18 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/bootstrap.js',
+            ],
             refresh: true,
         }),
     ],
+    define: {
+        'process.env': {
+            VITE_PUSHER_APP_KEY: process.env.VITE_PUSHER_APP_KEY,
+            VITE_PUSHER_APP_CLUSTER: process.env.VITE_PUSHER_APP_CLUSTER,
+        },
+    },
 });
