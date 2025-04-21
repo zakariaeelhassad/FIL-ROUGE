85e4ec0678adc46449e9ca80e4c10e44cb2d358a
zakariaeelhassad <zakariaelhassad031@example.com>
2025-04-21 16:13:35 +0100

    ajouter migration de chats

---
+++ Implementation/database/migrations/2025_04_21_091918_create_chats_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_one_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_two_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+
+            // Prevent duplicate chat between same two users
+            $table->unique(['user_one_id', 'user_two_id']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
