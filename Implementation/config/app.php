44727aa52c894ae0b0b8b1ccd989e6a55371c2f9
zakariaeelhassad <zakariaelhassad031@example.com>
2025-04-21 16:15:33 +0100

    ajouter dans app dans config le providers de la class BroadcastServiceProvider

--- Implementation/config/app.php
+++ Implementation/config/app.php
@@ -163,9 +163,10 @@ return [
         /*
          * Application Service Providers...
          */
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
+        App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ])->toArray(),
