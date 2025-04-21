ecf1e43710dfc69466749617d6742ce1141d25f3
zakariaeelhassad <zakariaelhassad031@example.com>
2025-04-21 16:18:32 +0100

    ajouter une model de message

---
+++ Implementation/app/Models/Message.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Message extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['chat_id', 'sender_id', 'receiver_id', 'message', 'read'];
+
+    protected $casts = [
+        'read' => 'boolean',
+    ];
+
+    public function chat()
+    {
+        return $this->belongsTo(Chat::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
