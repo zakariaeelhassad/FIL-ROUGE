bd93977f0ca67d681e97b86d778dd41e1d3d1194
zakariaeelhassad <zakariaelhassad031@example.com>
2025-04-24 14:25:14 +0100

    ajouter une megration pour les media de post

---
+++ Implementation/database/migrations/2025_04_22_202738_create_post_media_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_media', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->string('path');
+            $table->enum('type', ['image', 'video']);
+            $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_media');
+    }
+};
