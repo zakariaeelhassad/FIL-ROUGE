e0272c612f8b91679446216780a3d69a7110e627
zakariaeelhassad <zakariaelhassad031@example.com>
2025-04-24 16:16:54 +0100

    ajouter la function showProfil

--- Implementation/app/Http/Controllers/ProfilController.php
+++ Implementation/app/Http/Controllers/ProfilController.php
@@ -8,12 +8,65 @@ use Illuminate\Support\Facades\Auth;
 
 class ProfilController extends Controller
 {
+
+
+    public function profil(){
+        $user = auth()->user();
+        
+        if (!auth()->check()) {
+            return redirect()->route('login')->with('error', 'Veuillez vous connecter pour accéder à ce profil.');
+        }
+    
+        if ($user->role == 'joueur') {
+            $experiences = $user->experiences()->with('user')->latest()->get();  
+            $profile = \App\Models\JoueurProfile::where('user_id', $user->id)->first();
+            $posts = $user->posts()->with('user')->latest()->get();
+            $socialMedia = $user->socialMedia ?? null;
+    
+            return view('pages.profil_joueur', compact('user', 'posts', 'profile' , 'experiences' , 'socialMedia'));
+        }
+    
+        if ($user->role == 'club_admin') {
+            $profile = \App\Models\ClubAdminProfile::where('user_id', $user->id)->first();
+            $titres = \App\Models\Titre::where('user_id', $user->id)->latest()->get();
+            $posts = $user->posts()->with('user')->latest()->get();
+            $socialMedia = $user->socialMedia ?? null;
+    
+            return view('pages.profil_club_manager', compact('user', 'posts', 'profile', 'titres' , 'socialMedia'));
+        }
+    
+        return redirect()->route('home')->with('error', 'Role not found');
+    }
+
+    public function showprofil($id) {
+        $user = User::findOrFail($id);
+        
+        if ($user->role === 'club_admin') {
+            $profile = \App\Models\ClubAdminProfile::where('user_id', $user->id)->first();
+            $titres = \App\Models\Titre::where('user_id', $user->id)->latest()->get();
+            $posts = $user->posts()->with('user')->latest()->get();
+            $socialMedia = $user->socialMedia ?? null;
+    
+            return view('pages.profil_club_manager', compact('user', 'posts', 'profile', 'titres' , 'socialMedia'));
+        } 
+        elseif ($user->role === 'joueur') {
+            $experiences = $user->experiences()->with('user')->latest()->get();  
+            $profile = \App\Models\JoueurProfile::where('user_id', $user->id)->first();
+            $posts = $user->posts()->with('user')->latest()->get();
+            $socialMedia = $user->socialMedia ?? null;
+    
+            return view('pages.profil_joueur', compact('user', 'posts', 'profile' , 'experiences' , 'socialMedia'));
+        }
+        else {
+            return view('page.profil_default', compact('user'));
+        }
+    }
+
     public function show($id)
     {
         $user = User::find($id);
 
         if (!$user) {
-            // Redirect back with error message if user not found
             return redirect()->back()->with('error', 'Utilisateur non trouvé.');
         }
 
