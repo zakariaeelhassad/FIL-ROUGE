ee9d88a14119fe7ed72742099b96fff49cf45ef1
zakariaeelhassad <zakariaelhassad031@example.com>
2025-04-27 14:06:31 +0100

    create une migration pour les reports des posts , commenters et users

---
+++ Implementation/database/migrations/2025_04_25_141743_create_reports_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('reporter_id');
+            $table->string('reported_type'); 
+            $table->unsignedBigInteger('reported_id'); 
+            $table->text('reason')->nullable();
+            $table->timestamps();
+
+            $table->foreign('reporter_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reports');
+    }
+};
