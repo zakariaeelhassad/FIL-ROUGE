b194ea8c5af4e3cfd6d691e4cf20e28c27c0e74b
zakariaeelhassad <zakariaelhassad031@example.com>
2025-04-29 23:03:40 +0100

    ajouter une icon dans la component de tactique

--- Implementation/resources/views/components/profil/profil_club_admin/tactique.blade.php
+++ Implementation/resources/views/components/profil/profil_club_admin/tactique.blade.php
@@ -19,9 +19,7 @@
         <div class="flex justify-between items-center mb-4">
             <h3 class="text-lg font-semibold text-gray-900">Edit Tactique</h3>
             <button type="button" onclick="closeModal('tactiqueModal')" class="text-gray-500 hover:text-gray-700">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
+                <i class="fas fa-times h-5 w-5"></i>
             </button>
         </div>
         
